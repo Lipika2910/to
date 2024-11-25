f6dd47aa5f5db679652cf5a8e3aa9948ad1544b4
Lipika2910 <143777773+Lipika2910@users.noreply.github.com>
2024-11-25 21:02:59 +0530

    Add files via upload

---
+++ toc_sem5_FA_1.cpp
@@ -0,0 +1,91 @@
+//ques-1
+#include <iostream>
+using namespace std;
+
+int state1(string w, int i);
+int state2(string w, int i);
+int state3(string w, int i);
+int state4(string w, int i);
+
+int main(){
+	string w;
+	cout<<"enter string : ";
+	cin>>w;
+	state1(w,0);
+}
+
+int state1(string w, int i){
+	//cout<<"\nstate 1\n";
+	
+	if(i==w.length()){
+		cout<<"\nstring is rejected \n";
+		return 0;
+	}else{
+		
+		if(w[i]=='0'){
+			state1(w,i+1);
+		}
+		
+		if(w[i]=='1'){
+			state2(w,i+1);
+		}
+	}
+}
+
+
+int state2(string w, int i){
+	//cout<<"\nstate 2\n";
+	
+	if(i==w.length()){
+		cout<<"\nstring is rejected \n";
+		return 0;
+	}else{
+		
+		if(w[i]=='0'){
+			state1(w,i+1);
+		}
+		
+		if(w[i]=='1'){
+			state3(w,i+1);
+		}
+	}
+}
+
+
+int state3(string w, int i){
+	//cout<<"\nstate 3\n";
+	
+	if(i==w.length()){
+		cout<<"\nstring is rejected \n";
+		return 0;
+	}else{
+		
+		if(w[i]=='0'){
+			state1(w,i+1);
+		}
+		
+		if(w[i]=='1'){
+			state4(w,i+1);
+		}
+	}
+}
+
+
+int state4(string w, int i){
+	//cout<<"\nstate 4\n";
+	
+	if(i==w.length()){
+		cout<<"\nstring is accepted \n";
+		return 0;
+	}else{
+		
+		if(w[i]=='0'){
+			state4(w,i+1);
+		}
+		
+		if(w[i]=='1'){
+			state4(w,i+1);
+		}
+	}
+}
+
